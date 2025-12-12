7ea95492b45c378d4e3bc2eda57eb211799a4f8c
Агзамов Владимир <vladimir.agzamov@gazprombank.ru>
2025-12-12 23:04:29 +0300

    Initial commit

---
+++ CSharp-learning/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharp_learning;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        
+        Console.WriteLine("Привет, Мир!");
+    }
+}
