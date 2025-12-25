48896cc67aff06a47c822a58287ee8d33440551a
Агзамов Владимир <vladimir.agzamov@gazprombank.ru>
2025-12-25 15:01:34 +0300

    Add lesson variable

---
+++ Lesson4_Variable/VariableLesson.cs
@@ -0,0 +1,36 @@
+﻿namespace VariableLesson
+{
+    class Variable
+    {
+        static void Main (string[] args)
+        {
+            int a = 3;
+
+            Console.WriteLine(a);
+
+            a = 4;
+            
+            Console.WriteLine(a);
+            
+            int e,b,c;
+            
+            e = 6;
+            b = 7;
+            c = 8;
+            
+            Console.WriteLine(e + b + c);
+            
+            bool variable = true;
+            
+            Console.WriteLine(variable);
+
+            char z = '#';
+            
+            Console.WriteLine(z);
+            
+            string name = "Vova";
+            
+            Console.WriteLine(name);
+        }
+    }
+}
