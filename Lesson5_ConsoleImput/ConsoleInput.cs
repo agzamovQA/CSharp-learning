6343020a232d0c7f43c158b4456c8c84112a0db6
Агзамов Владимир <vladimir.agzamov@gazprombank.ru>
2025-12-26 17:42:00 +0300

    Add consoleInput program

---
+++ Lesson5_ConsoleImput/ConsoleInput.cs
@@ -0,0 +1,13 @@
+﻿namespace Lesson5_ConsoleImput;
+
+class ConsoleInput
+{
+    static void Main(string[] args)
+    {
+        string userName = Console.ReadLine();
+        
+        Console.ReadLine();
+
+        Console.WriteLine("Имя пользователя: " + userName);
+    }
+}
