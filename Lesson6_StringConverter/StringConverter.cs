7513cdcd6e5600470a13bfc74dd887a5abf6a82c
Агзамов Владимир <vladimir.agzamov@gazprombank.ru>
2025-12-26 18:53:23 +0300

    Add simple converter for HW

---
+++ Lesson6_StringConverter/StringConverter.cs
@@ -0,0 +1,28 @@
+﻿namespace Lesson6_StringConverter;
+
+class StringConverter
+{
+    static void Main(string[] args)
+    {
+        string userInput;
+
+        int a, b;
+
+        Console.WriteLine("Введите число 1");
+        
+        userInput = Console.ReadLine();
+        
+        a = Convert.ToInt32(userInput);
+
+        Console.WriteLine("Введите число 2");
+        
+        userInput = Console.ReadLine();
+        
+        b = Convert.ToInt32(userInput);
+
+        int result = a + b;
+        
+        Console.WriteLine("Результат сложения" + result);
+
+    }
+}
