1c10f14add30ef8e6d60256665efc2f80a5de39a
Агзамов Владимир <vladimir.agzamov@gazprombank.ru>
2025-12-29 13:18:03 +0300

    Add Arithmetic Operator

---
+++ Lesson8_ArithmeticOperations/ArithmeticOperations.cs
@@ -0,0 +1,26 @@
+﻿namespace Lesson8_ArithmeticOperations;
+
+class ArithmeticOperations
+{
+    static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 3;  
+        
+        int resultSumm = a + b;
+        Console.WriteLine("Результат сложения: " + resultSumm);
+        
+        int resultSubtraction = a - b;
+        Console.WriteLine("Результат вычитания: " + resultSubtraction);       
+        
+        int resultMultiplication = a * b;
+        Console.WriteLine("Результат умножения: " + resultMultiplication);
+        
+        int resultDivision = a / b;
+        Console.WriteLine("Результат деления: " + resultDivision);
+        
+        int resultDivisionRemainder = a % b;
+        Console.WriteLine("Результатом деления является остаток: " + resultDivisionRemainder);
+        
+    }
+}
