41453156bf4c56d3d05d71de5b0fadad25876484
Агзамов Владимир <vladimir.agzamov@gazprombank.ru>
2026-01-12 19:48:29 +0300

    Add IncrementAndDecrement

---
+++ Lesson11_IncrementAndDecrement/IncrementAndDecrement.cs
@@ -0,0 +1,19 @@
+﻿namespace Lesson11_IncrementAndDecrement;
+
+class IncrementAndDecrement
+{
+    static void Main(string[] args)
+    {
+        int a = 0;
+        int b = 0;
+
+        a = a++; // постфиксный инкримент
+        a = ++a; // префиксный инкримент
+
+        b = b--; // постфиксный декримент
+        b = --b; // префиксный декримент
+
+        Console.WriteLine("Префиксный инкримент " + ++a);
+        Console.WriteLine("Постфиксный инкримент " + b--);
+    }
+}
