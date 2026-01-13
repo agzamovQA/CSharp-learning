ee8f9c30e0bf279e3aaeba8946d1b987fc51e664
Агзамов Владимир <vladimir.agzamov@gazprombank.ru>
2026-01-13 08:47:32 +0300

    Add example If and Else

---
+++ Lesson13_IfElse/IfElseExample.cs
@@ -0,0 +1,21 @@
+﻿namespace Lesson13_IfElse;
+
+class IfElseExample
+{
+    static void Main(string[] args)
+    {
+        int userValue;
+        
+        userValue = int.Parse(Console.ReadLine());
+
+        if (userValue == 5)
+        {
+            Console.WriteLine("Ваше значение равно 5");
+        }
+        else
+        {
+            Console.WriteLine("Ваше значение НЕ равно 5");
+        }
+
+    }
+}
