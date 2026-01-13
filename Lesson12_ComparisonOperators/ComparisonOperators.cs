0ae7a3b69c3b4a41d3e9d75513a6eedf80495ce0
Агзамов Владимир <vladimir.agzamov@gazprombank.ru>
2026-01-13 07:50:29 +0300

    Add comparison operators example

---
+++ Lesson12_ComparisonOperators/ComparisonOperators.cs
@@ -0,0 +1,29 @@
+﻿
+/*
+    == Равно
+    != Не равно
+    > Больше
+    < Меньше
+    >= Больше или равно
+    <= Меньше или равно
+ */
+
+
+namespace Lesson12_ComparisonOperators;
+
+class ComparisonOperators
+{
+    static void Main(string[] args)
+    {
+        int firstValue = 5;
+        int secondValue = 10;
+        
+        
+        Console.WriteLine("Число 5 равно числу 10: " + (firstValue == secondValue));
+        Console.WriteLine("Число 5 Не равно числу 10: " + (firstValue != secondValue));
+        Console.WriteLine("Число 5 больше числа 10: " + (firstValue > secondValue));
+        Console.WriteLine("Число 5 меньше числа 10: " + (firstValue < secondValue));
+        Console.WriteLine("Число 5 больше или равно числу 10: " + (firstValue >= secondValue));
+        Console.WriteLine("Число 5 меньше или равно числу 10: " + (firstValue <= secondValue));
+    }
+}
