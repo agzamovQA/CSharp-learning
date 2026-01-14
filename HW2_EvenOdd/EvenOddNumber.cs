ec046db70e7cb04bfe9fb22152ebb9198154c98a
Агзамов Владимир <vladimir.agzamov@gazprombank.ru>
2026-01-14 18:05:06 +0300

    Add HomeWork 2

---
+++ HW2_EvenOdd/EvenOddNumber.cs
@@ -0,0 +1,23 @@
+﻿namespace HW2_EvenOdd;
+
+class EvenOddNumber
+{
+    static void Main(string[] args)
+    {
+        int userValue;
+
+        Console.WriteLine("Введите число, для определения четности: \n");
+        
+        userValue = int.Parse(Console.ReadLine());
+
+        if (userValue % 2 == 0)
+        {
+            Console.WriteLine("Вы ввели четное число");
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели нечетное число");
+        }
+
+    }
+}
