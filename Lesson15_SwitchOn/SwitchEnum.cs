422c8f85889c57a1170970d56df65a404fb5aa76
Агзамов Владимир <vladimir.agzamov@gazprombank.ru>
2026-01-16 18:32:49 +0300

    Add Switch consoleKey example

---
+++ Lesson15_SwitchOn/SwitchEnum.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace Lesson15_SwitchOn;
+
+class SwitchEnum
+{
+    static void Mai(string[] args)
+    {
+        ConsoleKey consoleKey = Console.ReadKey().Key;
+
+        switch (consoleKey)
+        {
+            // Alt + Enter -- "Generate switch cases"
+            case ConsoleKey.D0:
+                Console.WriteLine("Введена цифра 0");
+                break;
+            case ConsoleKey.D1:
+                Console.WriteLine("Введена цифра 1");
+                break;
+            case ConsoleKey.D2:
+                Console.WriteLine("Введена цифра 2");
+                break;
+            case ConsoleKey.D3:
+                Console.WriteLine("Введена цифра 3");
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
