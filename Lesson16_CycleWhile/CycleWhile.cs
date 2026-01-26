949516531ebc06806d886b5d5b0ba9eba3051956
Агзамов Владимир <vladimir.agzamov@gazprombank.ru>
2026-01-26 23:27:47 +0300

    Add folder lesson16

---
+++ Lesson16_CycleWhile/CycleWhile.cs
@@ -0,0 +1,13 @@
+﻿namespace Lesson16_CycleWhile;
+
+    /*
+     * Цикл while
+     */
+
+class CycleWhile
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
