a538839cd59f340772bb6780948a869cf65b4289
Агзамов Владимир <vladimir.agzamov@gazprombank.ru>
2026-02-02 19:04:09 +0300

    Добавлен подсчет суммы четных/нечетных чисел

--- HW4_RangeOfEvenAndOddNumbers/RangeOfEvenAndOddNumbers.cs
+++ HW4_RangeOfEvenAndOddNumbers/RangeOfEvenAndOddNumbers.cs
@@ -15,23 +15,30 @@ class RangeOfEvenAndOddNumbers
         int originalLowestNumber = lowestNumber; 
         int originalHighestNumber = highestNumber;
 
+        int evenNumberSumm = 0;
+        int oddNumberSumm = 0;
+
         while (lowestNumber < highestNumber)
         {
             if (++lowestNumber % 2 == 0) //Использован префиксный инкремент, чтобы сначала прибавлял, а потом делил на 2
             {
                 even++;
+                evenNumberSumm += lowestNumber; // Пример сокращенной арифметической формы
                 Console.WriteLine(lowestNumber + " - четное число");
             }
             else
             {
                 odd++;
+                oddNumberSumm = oddNumberSumm + lowestNumber;
                 Console.WriteLine(lowestNumber + " - нечетное число");
             }
         }
         
         Console.WriteLine("\nИтоги для диапазона от " + originalLowestNumber + " до " + originalHighestNumber);
         Console.WriteLine("Всего четных: " + even);
-        Console.WriteLine("Всего нечетных: " + odd);
+        Console.WriteLine("Всего нечетных: " + odd);        
+        Console.WriteLine("Сумма четных: " + evenNumberSumm);
+        Console.WriteLine("Сумма нечетных: " + oddNumberSumm);
         
         Console.WriteLine("\nНажмите любую клавишу для выхода...");
         Console.ReadKey();
