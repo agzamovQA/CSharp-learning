585f888483a4dffe538a33b1f2f4c8b41589063e
Agzamov Vladimir <agzamov.qa@yandex.ru>
2026-02-27 18:05:10 +0300

    Add example cycle for

---
+++ Lesson18_CycleFor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace Lesson18_CycleFor;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int limit = int.Parse(Console.ReadLine());
+
+        // i - переменная, i < limit - условие. Выполняй ДО, i++ -- что выполнять(операция).
+        for (int i = 0; i < limit; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
